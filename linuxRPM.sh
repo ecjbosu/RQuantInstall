7986cd579dd0c2c496464f206cd282e001a48c8b
Joe W. Byers <ecjbosu@aol.com>
2015-11-13 17:05:52 -0500

    added new jar packages to rpm install.

--- linuxRPM.sh
+++ linuxRPM.sh
@@ -4,6 +4,7 @@
 
 
   yum install QuantLib QuantLib-devel geos geos-devel gsl gsl-devel netcdf netcdf-devel
-  gdal gdal-* mpfr mfpr-devel postgresql-* lapack-* blas-* ununits-* udunits2-* fftw-*
-  libiodbc-* unixODBC-* oroj-*
+    gdal gdal-* mpfr mfpr-devel postgresql-* lapack-* blas-* ununits-* udunits2-* fftw-*
+    libiodbc-* unixODBC-* oroj-* ssparser nekohtml htmlunit htmlunit-core-js jsoup json-lib
+    ezmorph tagsoup sac
  
