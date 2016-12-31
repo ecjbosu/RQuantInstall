2ecc6ca1b7a79461cd33e440c8ee616d16ea51f6
Joe W. Byers <ecjbosu@aol.com>
2016-12-31 05:10:00 -0500

    This is a major cleanup of packages and dependencies
    RPM shell added curl-* glpk-* coin-or-SYMPHOMY-* libxml2-devel openssl-devel gmp-* 
        libjpeg-turbo-*  gtk2-devel ggobi-devel libXt-devel postgresql-devel gdal-devel 
        proj-devel mesa-libGLU-devel mesa-libGL-devel lpsolve-devel
    
    package clean up include removing package not in cran anymore, packages for SciViews and others that would be installed dependency checks.  Packages included Gtk related, rpanel, all sv* packages, rdroolsjars, wq, cache.Sweave, mutatr, Rjmsjars, fabia, rsqllite.extfunc, ncdf, r.archive, r.menu.
    
    Move quantmod, rgl, lpsolve, lpsolveapi, and performanceanalytics to currentPacks.  These are needed.

--- linuxRPM.sh
+++ linuxRPM.sh
@@ -3,8 +3,15 @@
 
 
 
-  yum install QuantLib QuantLib-devel geos geos-devel gsl gsl-devel netcdf netcdf-devel
-    gdal gdal-* mpfr mfpr-devel postgresql-* lapack-* blas-* ununits-* udunits2-* fftw-*
-    libiodbc-* unixODBC-* oroj-* ssparser nekohtml htmlunit htmlunit-core-js jsoup json-lib
-    ezmorph tagsoup sac
+#yum install QuantLib QuantLib-devel geos geos-devel gsl gsl-devel netcdf netcdf-devel \
+#    gdal gdal-* mpfr mfpr-devel postgresql-* lapack-* blas-* ununits-* udunits2-* fftw-* \
+#    libiodbc-* unixODBC-* oroj-* ssparser nekohtml htmlunit htmlunit-core-js jsoup json-lib \
+#    ezmorph tagsoup sac curl-* glpk-* coin-or-SYMPHOMY-* libxml2-devel openssl-devel gmp-* \
+ 
+dnf install QuantLib QuantLib-devel geos geos-devel gsl gsl-devel netcdf netcdf-devel \
+    gdal gdal-* mpfr mfpr-devel postgresql-* lapack-* blas-* ununits-* udunits2-* fftw-* \
+    libiodbc-* unixODBC-* oroj-* ssparser nekohtml htmlunit htmlunit-core-js jsoup json-lib \
+    ezmorph tagsoup sac curl-* glpk-* coin-or-SYMPHOMY-* libxml2-devel openssl-devel gmp-* \
+    libjpeg-turbo-*  gtk2-devel ggobi-devel libXt-devel postgresql-devel gdal-devel \
+    proj-devel mesa-libGLU-devel mesa-libGL-devel lpsolve-devel
  
