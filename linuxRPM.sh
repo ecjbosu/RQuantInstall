44e885fb90dd2d11f97c0b9c82dcffe8ec4b03b6
Joe W. Byers <ecjbosu@aol.com>
2019-09-01 09:36:07 -0500

    Update linuxRPM.sh
    removed package name for all but QuantLIb since installing devels will install base package.   Removed gtk2-devel, need to see if gtk3-devel works.  This is for R packages with a GUI.

--- linuxRPM.sh
+++ linuxRPM.sh
@@ -8,12 +8,13 @@
 #    libiodbc-* unixODBC-* oroj-* ssparser nekohtml htmlunit htmlunit-core-js jsoup json-lib \
 #    ezmorph tagsoup sac curl-* glpk-* coin-or-SYMPHOMY-* libxml2-devel openssl-devel gmp-* \
  
-dnf install QuantLib QuantLib-devel geos geos-devel gsl gsl-devel netcdf netcdf-devel \
-    gdal gdal-* mpfr mpfr-devel postgresql-* lapack-* blas-* udunits2-* fftw-* \
-    libiodbc-* unixODBC-* cssparser nekohtml htmlunit htmlunit-core-js jsoup json-lib \
-    ezmorph tagsoup sac glpk-devel glpk-static glpk-utils coin-or-SYMPHONY-devel \
-    libxml2-devel openssl-devel gmp-* \
-    libjpeg-turbo-*  gtk2-devel ggobi-devel libXt-devel postgresql-devel gdal-devel \
-    proj-devel proj-epsg mesa-libGLU-devel mesa-libGL-devel lpsolve-devel libcurl-devel
+dnf install QuantLib QuantLib-devel geos-devel gsl-devel netcdf-devel \
+    gdal-devel mpfr-devel postgresql-devel lapack-devel blas-devel udunits2-devel \
+    fftw-devel libiodbc-devel unixODBC-devel cssparser nekohtml htmlunit htmlunit-core-js 
+    jsoup json-lib ezmorph tagsoup sac glpk-devel glpk-static glpk-utils coin-or-SYMPHONY-devel \
+    libxml2-devel openssl-devel gmp-devel libjpeg-turbo-devel ggobi-devel libXt-devel \ 
+    postgresql-devel gdal-devel proj-devel proj-epsg mesa-libGLU-devel mesa-libGL-devel \
+    lpsolve-devel libcurl-devel poppler-cpp-devel poppler-glib-devel ImageMagick-c++-devel cargo
+
     
- 
+ Removed:  gtk2-devel (need to see if supplies rpanel BWidgets) 
