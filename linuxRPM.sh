216b3f93c37dc0aad85eb67493bf7c6964337c13
ecjbosu <ecjbosu@aol.com>
2023-09-07 06:57:24 -0500

    Updates scripts to remove obsolete packages and others that are not needed.
    Signed-off-by: ecjbosu <ecjbosu@aol.com>

--- linuxRPM.sh
+++ linuxRPM.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 # R Quant Install rpm dependencies
 
-
-
-#yum install QuantLib QuantLib-devel geos geos-devel gsl gsl-devel netcdf netcdf-devel \
-#    gdal gdal-* mpfr mfpr-devel postgresql-* lapack-* blas-* ununits-* udunits2-* fftw-* \
-#    libiodbc-* unixODBC-* oroj-* ssparser nekohtml htmlunit htmlunit-core-js jsoup json-lib \
-#    ezmorph tagsoup sac curl-* glpk-* coin-or-SYMPHOMY-* libxml2-devel openssl-devel gmp-* \
- 
-dnf install QuantLib QuantLib-devel geos-devel gsl-devel netcdf-devel     gdal-devel mpfr-devel postgresql-devel lapack-devel blas-devel udunits2-devel  fftw-devel libiodbc-devel unixODBC-devel cssparser nekohtml htmlunit htmlunit-core-js jsoup json-lib ezmorph tagsoup sac glpk-devel glpk-static glpk-utils coin-or-SYMPHONY-devel libxml2-devel openssl-devel gmp-devel libjpeg-turbo-devel ggobi-devel libXt-devel postgresql-devel gdal-devel proj-devel proj-epsg mesa-libGLU-devel mesa-libGL-devel lpsolve-devel libcurl-devel poppler-cpp-devel poppler-glib-devel ImageMagick-c++-devel cargo libgit2-devel udunits2-devel protobuf-devel rust cargo ffmpeg-devel tesseract-devel librsvg2-devel ImageMagick-c++-devel 
+dnf install QuantLib QuantLib-devel geos-devel gsl-devel netcdf-devel gdal-devel mpfr-devel postgresql-devel lapack-devel blas-devel udunits2-devel fftw-devel libiodbc-devel unixODBC-devel nekohtml jsoup tagsoup sac glpk-devel glpk-utils coin-or-SYMPHONY-devel libxml2-devel openssl-devel gmp-devel libjpeg-turbo-devel libXt-devel postgresql-devel gdal-devel proj-devel mesa-libGLU-devel mesa-libGL-devel lpsolve-devel libcurl-devel poppler-cpp-devel poppler-glib-devel ImageMagick-c++-devel cargo libgit2-devel udunits2-devel protobuf-devel rust cargo tesseract-devel librsvg2-devel  
     
 # Removed:  gtk2-devel (need to see if supplies rpanel BWidgets) 
 # may need to add later to install packages list
